5788690baf4871d1a34fea70e53fcec1412b26f8
Aslıhan <erdallasll@gmail.com>
2026-03-09 14:17:30 +0300

    feat: use ctrlWrapper in contacts router

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -9,27 +9,28 @@ import {
 import { validateBody } from '../middlewares/validateBody.js';
 import { isValidId } from '../middlewares/isValidId.js';
 import { createContactSchema, updateContactSchema } from '../validation/contacts.js';
+import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 
 const router = Router();
 
 // Tüm kontakları getir (Sayfalandırma, sıralama, filtreleme dahil)
-router.get('/', getContactsController);
+router.get('/', ctrlWrapper(getContactsController));
 
 // Tek bir kontak getir (isValidId eklendi)
-router.get('/:contactId', isValidId, getContactByIdController);
+router.get('/:contactId', isValidId, ctrlWrapper(getContactByIdController));
 
 // Yeni kontak ekle (validateBody eklendi)
-router.post('/', validateBody(createContactSchema), createContactController);
+router.post('/', validateBody(createContactSchema), ctrlWrapper(createContactController));
 
 // Kontağı güncelle (isValidId ve validateBody eklendi)
 router.patch(
     '/:contactId',
     isValidId,
     validateBody(updateContactSchema),
-    updateContactController
+    ctrlWrapper(updateContactController)
 );
 
 // Kontağı sil (isValidId eklendi)
-router.delete('/:contactId', isValidId, deleteContactController);
+router.delete('/:contactId', isValidId, ctrlWrapper(deleteContactController));
 
 export default router;
